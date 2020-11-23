c61669cce11539a2d7a12a58ec1556c545b3ccc5
Guilherme Augusto Xavier de Faria <desenvolvimento01!@somalogistica.com.br>
2020-11-23 15:40:00 -0300

    Atualizando, criação de mascara CPF

---
+++ FI.AtividadeEntrevista/BLL/BoCliente.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoCliente
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public long Incluir(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Incluir(cliente);
+        }
+
+        /// <summary>
+        /// Altera um cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public void Alterar(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Alterar(cliente);
+        }
+
+        /// <summary>
+        /// Consulta o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public DML.Cliente Consultar(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Consultar(id);
+        }
+
+        /// <summary>
+        /// Excluir o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Listar()
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Listar();
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Pesquisa(iniciarEm,  quantidade, campoOrdenacao, crescente, out qtd);
+        }
+
+        /// <summary>
+        /// VerificaExistencia
+        /// </summary>
+        /// <param name="CPF"></param>
+        /// <returns></returns>
+        public bool VerificarExistencia(string CPF)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.VerificarExistencia(CPF);
+        }
+
+        public bool ValidarCPF(string CPF)
+        {
+            string valorCpf = CPF.Replace(".", "");
+
+            valorCpf = valorCpf.Replace("-", "");
+
+            if (valorCpf.Length != 11)
+                return false;
+
+            bool valido = true;
+
+            for (int i = 1; i < 11 && valido; i++)
+
+                if (valorCpf[i] != valorCpf[0])
+                    valido = false;
+
+            if (valido || valorCpf == "12345678909" || valorCpf == "00000000000")
+                return false;
+
+
+            int[] numeros = new int[11];
+
+            for (int i = 0; i < 11; i++)
+
+                numeros[i] = int.Parse(
+                    valorCpf[i].ToString());
+
+            int soma = 0;
+
+
+            for (int i = 0; i < 9; i++)
+
+                soma += (10 - 1) * numeros[i];
+
+            int resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[9] != 0)
+
+
+                    return false;
+
+            }
+            else if (numeros[9] != 11 - resultado)
+
+                return false;
+
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+
+
+                soma += (11 - i) * numeros[i];
+
+            resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[10] != 0)
+                    return false;
+            }
+
+            else
+                if (numeros[10] != 11 - resultado)
+
+                return false;
+
+            return true;
+
+        }
+    }
+}
